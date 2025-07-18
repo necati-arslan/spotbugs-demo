38557753b3235397cbaf291b0bdae51260996f2e
Necati Arslan <necati.arslan2018@gmail.com>
2025-07-18 15:27:35 +0200

    this-code-works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,16 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         // 🔸 1. toUpperCase without locale
-        String name = "çağrı";
-        System.out.println(name.toUpperCase()); // ❌ SpotBugs: DM_CONVERT_CASE
+//        String name = "çağrı";
+//        System.out.println(name.toUpperCase()); // ❌ SpotBugs: DM_CONVERT_CASE
+//
+//        // 🔸 2. Hard-coded encoding
+//        byte[] data = "İstanbul".getBytes(); // ❌ SpotBugs: DM_DEFAULT_ENCODING
+//
+//        // 🔸 3. Date format without locale
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT); // ❌ SpotBugs: DATE_FORMAT_WITHOUT_LOCALE
+//        System.out.println(df.format(new Date()));
 
-        // 🔸 2. Hard-coded encoding
-        byte[] data = "İstanbul".getBytes(); // ❌ SpotBugs: DM_DEFAULT_ENCODING
-
-        // 🔸 3. Date format without locale
-        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT); // ❌ SpotBugs: DATE_FORMAT_WITHOUT_LOCALE
-        System.out.println(df.format(new Date()));
+        System.out.printf("Hello World!\n");
     }
 }
