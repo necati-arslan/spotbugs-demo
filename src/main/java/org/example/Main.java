53c3d04961b7e3bdf9e93f6a6823c7e753548e02
Necati Arslan <necati.arslan2018@gmail.com>
2025-07-18 15:59:03 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,17 +7,6 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // 🔸 1. toUpperCase without locale
-//        String name = "çağrı";
-//        System.out.println(name.toUpperCase()); // ❌ SpotBugs: DM_CONVERT_CASE
-//
-//        // 🔸 2. Hard-coded encoding
-//        byte[] data = "İstanbul".getBytes(); // ❌ SpotBugs: DM_DEFAULT_ENCODING
-//
-//        // 🔸 3. Date format without locale
-//        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT); // ❌ SpotBugs: DATE_FORMAT_WITHOUT_LOCALE
-//        System.out.println(df.format(new Date()));
-
         System.out.printf("Hello World!\n");
     }
 }
