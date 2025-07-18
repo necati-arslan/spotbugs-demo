c8a3fa2794d1f64ae2276f08d5ef3ba3dd83ad9c
Necati Arslan <necati.arslan2018@gmail.com>
2025-07-18 16:02:03 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!\n");
+        System.out.println("Hello World!\n");
     }
 }
