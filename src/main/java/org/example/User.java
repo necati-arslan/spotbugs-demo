421fb90362e0afbd610990d8a87b54284867964a
Necati Arslan <necati.arslan2018@gmail.com>
2025-07-30 08:32:50 +0200

    added EI_EXPOS_REP

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private final List<String> roles = new ArrayList<>();
+
+    public User() {
+        roles.add("USER");
+    }
+
+    public List<String> getRoles() {
+        // ❌ SpotBugs burada EI_EXPOSE_REP uyarısı verir
+        return roles;
+        //return new ArrayList<>(roles);
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+
+        // getRoles ile içteki listeye doğrudan erişim ve değişiklik
+        List<String> exposed = user.getRoles();
+        exposed.add("ADMIN");  // User nesnesinin iç durumu dışarıdan değiştirildi!
+
+        System.out.println("User roles: " + user.getRoles()); // USER, ADMIN
+
+        User user2 = new User();
+
+        // getRoles ile içteki listeye doğrudan erişim ve değişiklik
+        System.out.println("User roles: " + user2.getRoles()); // USER, ADMIN
+
+    }
 }
